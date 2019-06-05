14652521f158f6f4eae20d941848407311331ed6
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-06-05 22:01:29 +0200

    switch image toevoegen

--- client/src/components/user/VertaalForm.jsx
+++ client/src/components/user/VertaalForm.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import switchImg from "../../assets/user/switch.png";
+
 const VertaalForm = () => {
   return (
     <>
       <p>Nederlands</p>
-      <img src="" alt="switch" />
+      <img src={switchImg} alt="switch" width="50" height="50" />
       <p>Engels</p>
       <input type="text" value="Ik ken geen andere taal" />
       <p>I don't know any other languages</p>
