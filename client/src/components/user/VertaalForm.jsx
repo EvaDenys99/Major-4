861c6bc5de0cb6b7d2296bcc4ed76daa17c02258
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-06-06 16:30:38 +0200

    vertaling state werkt

--- client/src/components/user/VertaalForm.jsx
+++ client/src/components/user/VertaalForm.jsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 
 import switchImg from "../../assets/user/switch.png";
 
-const VertaalForm = () => {
-  return (
-    <>
-      <select name="" id="">
-        <option value="Nederlands" />
-      </select>
-      <img src={switchImg} alt="switch" width="50" height="50" />
-      <p>Engels</p>
-      <input type="text" value="Ik ken geen andere taal" />
-      <p>I don't know any other languages</p>
-      <button>Vertaal</button>
-    </>
-  );
-};
+class VertaalForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { vertaald: false };
+  }
+
+  handleSubmitVertaal = e => {
+    e.preventDefault();
+    this.setState({ vertaald: true });
+  };
+
+  handleSubmitReset = e => {
+    e.preventDefault();
+    this.setState({ vertaald: false });
+  };
+
+  render() {
+    const { vertaald } = this.state;
+
+    return vertaald ? (
+      <>
+        <select name="" id="">
+          <option value="Nederlands">Nederlands</option>
+        </select>
+        <img src={switchImg} alt="switch" width="50" height="50" />
+        <select name="" id="">
+          <option value="Engels">Engels</option>
+        </select>
+        <div>
+          <input type="text" defaultValue="Ik ken geen andere taal" />
+          <p>I don't know any other languages</p>
+        </div>
+        <button onClick={this.handleSubmitReset}>Opnieuw</button>
+      </>
+    ) : (
+      <>
+        <select name="" id="">
+          <option value="Nederlands">Nederlands</option>
+        </select>
+        <img src={switchImg} alt="switch" width="50" height="50" />
+        <select name="" id="">
+          <option value="Engels">Engels</option>
+        </select>
+        <div>
+          <input type="text" defaultValue="Ik ken geen andere taal" />
+        </div>
+        <button onClick={this.handleSubmitVertaal}>Vertaal</button>
+      </>
+    );
+  }
+}
 
 export default VertaalForm;
