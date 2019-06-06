8707be276d4e641897eab351ef126ece9cf1bf35
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-06-06 16:53:20 +0200

    locaties uitklapbaar gemaakt

--- client/src/containers/user/Home.jsx
+++ client/src/containers/user/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 import { Link } from "react-router-dom";
 
@@ -6,35 +6,94 @@ import NavigatieHome from "../../components/user/NavigatieHome";
 
 // zoals editshort van opdrachtWAD -> als er geklikt wordt op de 'andere' worden de buttons weergeven
 
-const Home = ({ zaalStore }) => {
-  return (
-    <>
-      {zaalStore.hoofdZalen.map(zaal => {
-        return (
-          <Link to={`zaal/${zaal.id}`} key={zaal.id}>
-            <div>
-              <img src={zaal.img} alt={zaal.naam} />
-            </div>
-          </Link>
-        );
-      })}
+// const Home = ({ zaalStore }) => {
+//   return (
+//     <>
+//       {zaalStore.hoofdZalen.map(zaal => {
+//         return (
+//           <Link to={`zaal/${zaal.id}`} key={zaal.id}>
+//             <div>
+//               <img src={zaal.img} alt={zaal.naam} />
+//             </div>
+//           </Link>
+//         );
+//       })}
 
-      {zaalStore.bijZalen.map(zaal => {
-        return (
-          <Link to={`zaal/${zaal.id}`} key={zaal.id}>
-            <div>
-              <p>{zaal.naam}</p>
-            </div>
-          </Link>
-        );
-      })}
-      {/* button andere */}
-      {/* OF */}
-      {/* button andere */}
-      {/* forEach (hoofdZalen as hoofdZaal) => {<Link to="idk"><p>AMUZ</p></Link>} */}
-      <NavigatieHome />
-    </>
-  );
-};
+//       {zaalStore.bijZalen.map(zaal => {
+//         return (
+//           <Link to={`zaal/${zaal.id}`} key={zaal.id}>
+//             <div>
+//               <p>{zaal.naam}</p>
+//             </div>
+//           </Link>
+//         );
+//       })}
+//       <NavigatieHome />
+//     </>
+//   );
+// };
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { uitgeklapt: false };
+  }
+
+  handleSubmitMeer = e => {
+    e.preventDefault();
+    console.log(this.props);
+    this.setState({ uitgeklapt: true });
+  };
+
+  handleSubmitMinder = e => {
+    e.preventDefault();
+    console.log(this.props);
+    this.setState({ uitgeklapt: false });
+  };
+
+  render() {
+    const { zaalStore } = this.props;
+    const { uitgeklapt } = this.state;
+
+    return uitgeklapt ? (
+      <>
+        {zaalStore.hoofdZalen.map(zaal => {
+          return (
+            <Link to={`zaal/${zaal.id}`} key={zaal.id}>
+              <div>
+                <img src={zaal.img} alt={zaal.naam} />
+              </div>
+            </Link>
+          );
+        })}
+        <button onClick={this.handleSubmitMinder}>Andere locaties</button>
+        {zaalStore.bijZalen.map(zaal => {
+          return (
+            <Link to={`zaal/${zaal.id}`} key={zaal.id}>
+              <div>
+                <p>{zaal.naam}</p>
+              </div>
+            </Link>
+          );
+        })}
+        <NavigatieHome />
+      </>
+    ) : (
+      <>
+        {zaalStore.hoofdZalen.map(zaal => {
+          return (
+            <Link to={`zaal/${zaal.id}`} key={zaal.id}>
+              <div>
+                <img src={zaal.img} alt={zaal.naam} />
+              </div>
+            </Link>
+          );
+        })}
+        <button onClick={this.handleSubmitMeer}>Andere locaties</button>
+        <NavigatieHome />
+      </>
+    );
+  }
+}
 
 export default inject(`zaalStore`)(observer(Home));
