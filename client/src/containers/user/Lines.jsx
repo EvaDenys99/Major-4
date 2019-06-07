ccb45c261566ba0272ff6d9c3077ccf0862b4b1d
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-06-07 18:11:50 +0200

    probeersel fixen messages doorgeven server naar client

--- client/src/containers/user/Lines.jsx
+++ client/src/containers/user/Lines.jsx
@@ -2,28 +2,26 @@ import React from "react";
 
 import NavigatieLines from "./../../components/user/NavigatieLines";
 
-const Vertaal = messages => {
+const Lines = messages => {
   console.log(messages);
   return (
     <>
       <h1>Push Lines Overzicht</h1>
       <p>Bekijk hier de vorige pushlines</p>
       <ul>
-        {/* {messages.map(message => (
-          <ul key={message}>
-            <li>{message}</li>
-          </ul>
-        ))} */}
-        <li>
+        {messages.messages.map(message => (
+          <li key={message}>{message}</li>
+        ))}
+        {/* <li>
           <p>De geit is een belangrijke plot twist, mis hem niet!</p>
         </li>
         <li>
           <p>Blablabla</p>
-        </li>
+        </li> */}
       </ul>
       <NavigatieLines />
     </>
   );
 };
 
-export default Vertaal;
+export default Lines;
