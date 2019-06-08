551aa198666c3c86e993466841d145000eeb4ad1
Ayko De Smedt <ayko.de.smedt@student.howest.be>
2019-06-08 17:55:47 +0200

    probeersels om messages uit array te halen met zaal

--- client/src/containers/user/Lines.jsx
+++ client/src/containers/user/Lines.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Select from "react-select";
 import NavigatieLines from "./../../components/user/NavigatieLines";
 import { decorate, observable } from "mobx";
 import { observer } from "mobx-react";
@@ -7,13 +8,14 @@ const io = require(`socket.io-client`);
 class Lines extends Component {
   constructor(props) {
     super(props);
+    // console.log(props);
     this.state = { messages: [] };
     const socket = io.connect(`:3000`);
     const messages = [];
 
     socket.on(`chat message`, function(msg) {
       messages.push(msg);
-      window.scrollTo(0, document.body.scrollHeight);
+      // window.scrollTo(0, document.body.scrollHeight);
       console.log(messages);
       stateAanpassing(messages);
       return messages;
@@ -29,13 +31,39 @@ class Lines extends Component {
       return <li key={index}>{message}</li>;
     };
 
+    const handleSelection = e => {
+      console.log(e.value);
+      if (e.value === `antwerpen`) {
+        console.log(`antwerpen`);
+      }
+      console.log(messages);
+
+      //in principe zou dit toch moeten werken:
+      // const gefilterdeMessages = [];
+      // messages.map(message =>
+      //   e.value === message.zaal
+      //     ? gefilterdeMessages.push(message)
+      //     : console.log(`nope`)
+      // );
+
+      // this.setState({ messages: gefilterdeMessages });
+    };
+
     const { messages } = this.state;
+
+    const options = [
+      { label: `antwerpen`, value: `antwerpen` },
+      { label: `gent`, value: `gent` },
+      { label: `'t eilandje`, value: `eilandje` }
+    ];
 
     return (
       <>
-        {console.log(messages)}
-        <h1>Push Lines Overzicht</h1>
+        {/* {console.log(messages)} */}
+        <h1>Overzicht</h1>
         <p>Bekijk hier de vorige pushlines</p>
+        <Select name="" id="" onChange={handleSelection} options={options} />
+
         <ul id="messages" />
         <ul>
           {messages.length > 0 ? (
