91e78ed94ae2f299d70ae0cda0fcf348d1ee92ee
Eva Denys <eva.denys@student.howest.be>
2019-06-10 12:28:41 +0200

    progressbar bediening user

--- client/src/components/user/Progressbar.jsx
+++ client/src/components/user/Progressbar.jsx
@@ -24,7 +24,15 @@ class Progressbar extends Component {
   render() {
     return (
       <div>
-        <Progress percent={this.state.percentage} />
+        <Progress
+          percent={this.state.percentage}
+          theme={{
+            active: {
+              symbol: this.state.percentage + `%`,
+              color: `rgb(223, 105, 180)`
+            }
+          }}
+        />
         <div>
           <button onClick={this.nextStep}>Next Step</button>
           <button onClick={this.reset}>reset</button>
@@ -36,92 +44,3 @@ class Progressbar extends Component {
 }
 
 export default Progressbar;
-
-// import React, { Component } from "react";
-// //import "moment-duration-format";
-// import { CircularProgressbar } from "react-circular-progressbar";
-// // import Progressbar from "react-linear-progressbar";
-// import "react-circular-progressbar/dist/styles.css";
-
-// class Progress extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       status: false,
-//       break: false,
-//       runningTime: 0,
-//       startSession: 108,
-//       startBreak: 300
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//     this.handleReset = this.handleReset.bind(this);
-//   }
-
-//   handleClick() {
-//     this.setState(state => {
-//       if (state.status) {
-//         clearInterval(this.timer);
-//       } else if (!state.status) {
-//         this.timer = setInterval(() => {
-//           if (this.state.runningTime >= 1) {
-//             this.setState({ runningTime: this.state.runningTime + 1 });
-//           } else if (this.state.runningTime < 1 && !this.state.break) {
-//             this.setState({ runningTime: this.state.startBreak });
-//             this.setState({ break: true });
-//           } else if (this.state.runningTime <= 1 && this.state.break) {
-//             this.handleReset();
-//             clearInterval(this.timer);
-//           }
-//         }, 100);
-//       } else if (this.state.runningTime <= 1 && this.state.break) {
-//         this.handleReset();
-//         clearInterval(this.timer);
-//       }
-
-//       return { status: !state.status };
-//     });
-//   }
-
-//   handleReset() {
-//     this.setState({
-//       status: false,
-//       break: false,
-//       runningTime: 0,
-//       startSession: 10800,
-//       startBreak: 300
-//     });
-//     clearInterval(this.timer);
-//   }
-
-//   render() {
-//     let baseTime = this.state.startSession;
-//     let percentage = ((this.state.runningTime / baseTime) * 100).toFixed(0);
-
-//     return (
-//       <div>
-//         <div style={{ width: `25%` }}>
-//           <CircularProgressbar percentage={25} />
-//         </div>
-//         <div>
-//           <button
-//             className="btn btn-dark"
-//             id="start_stop"
-//             type="button"
-//             onClick={this.handleClick}
-//           >
-//             {this.state.status ? `Stop` : `Start`}
-//           </button>
-//           <button
-//             className="btn btn-dark"
-//             id="reset"
-//             type="button"
-//             onClick={this.handleReset}
-//           >
-//             Reset
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-// export default Progress;
