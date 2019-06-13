ec1b520d3cf33d726bfa319e3735e02f273cab72
Eva Denys <eva.denys@student.howest.be>
2019-06-13 13:24:43 +0200

    test

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose
 
 const app = express();
 
-const server = require("https").createServer(app);
+const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 io.on("connection", function(socket) {
