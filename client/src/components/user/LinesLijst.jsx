53950f0559f6939bf90e07cc694371fa78dfe332
Eva Denys <eva.denys@student.howest.be>
2019-06-13 18:26:42 +0200

    testtttt

--- client/src/components/user/LinesLijst.jsx
+++ client/src/components/user/LinesLijst.jsx
@@ -10,42 +10,17 @@ class BaseLinesLijst extends Component {
   constructor(props) {
     super(props);
     this.state = { messages: [] };
-    // this.socket = io.connect(`:${this.props.port}`);
-
-    // const messages = [];
-
-    // // this.socket.open();
-    // this.socket.on(`chat message`, function(msg) {
-    //   cogoToast.success(msg, {
-    //     position: `top-center`
-    //   });
-    //   messages.push(msg);
-    //   // console.log(messages);
-    //   stateAanpassing(messages);
-    //   return messages;
-    // });
-
-    // const stateAanpassing = messages => {
-    //   this.setState({ messages: messages });
-    //   return messages;
-    // };
-  }
-
-  // componentWillMount() {
-  //   this.socket.close();
-  //   this.socket.off(`chat message`);
-  // }
-
-  componentDidMount() {
     this.socket = io.connect(`:${this.props.port}`);
+
     const messages = [];
-    this.socket.open();
+
+    // this.socket.open();
     this.socket.on(`chat message`, function(msg) {
       cogoToast.success(msg, {
         position: `top-center`
       });
       messages.push(msg);
-      console.log(messages);
+      // console.log(messages);
       stateAanpassing(messages);
       return messages;
     });
@@ -55,6 +30,31 @@ class BaseLinesLijst extends Component {
       return messages;
     };
   }
+
+  // componentWillMount() {
+  //   this.socket.close();
+  //   this.socket.off(`chat message`);
+  // }
+
+  // componentDidMount() {
+  //   this.socket = io.connect(`:${this.props.port}`);
+  //   const messages = [];
+  //   this.socket.open();
+  //   this.socket.on(`chat message`, function(msg) {
+  //     cogoToast.success(msg, {
+  //       position: `top-center`
+  //     });
+  //     messages.push(msg);
+  //     console.log(messages);
+  //     stateAanpassing(messages);
+  //     return messages;
+  //   });
+
+  //   const stateAanpassing = messages => {
+  //     this.setState({ messages: messages });
+  //     return messages;
+  //   };
+  // }
 
   // componentWillUnmount() {
   //   this.socket.close();
